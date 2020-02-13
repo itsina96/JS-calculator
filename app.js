c66d6a273466bfc392f1380e2c3cb0a075c360a1
Ina <53597131+itsina96@users.noreply.github.com>
2020-02-13 22:14:54 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ for(let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function () {
     let buttonValue = this.textContent;
     if (buttonValue === "clear") {
-      display.innerHTML = "0000";
+      display.innerHTML = "0";
     }
     else {
       display.innerHTML = buttonValue;
