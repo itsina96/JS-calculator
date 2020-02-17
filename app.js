0c4460c31042f38fc76ba663e1c8caeca37fb190
Ina <53597131+itsina96@users.noreply.github.com>
2020-02-17 18:53:28 +0000

    console.log to make sure it listens for all keypresses

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.keys');
 
 keys.addEventListener('click', e => {
-const key = e.target
-const action = key.dataset.action
-
-if(!action) {
-  console.log('numer key!')
-}
+    const key = e.target
+    const action = key.dataset.action
+    
+    if(!action) {
+        console.log('numer key!')
+    }
+    
+    if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+        console.log('operator key!')
+    }
+    
+    if (action === 'decimal') {
+        console.log('decimal key!')
+    }
+    if(action === 'clear') {
+        console.log('clear key!')
+    }
+    if(action === 'calculate') {
+        console.log('equal key!')
+    }
+    
 });
