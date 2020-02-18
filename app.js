ec279a808d906c7f132266e6eca94f63e09f3412
Ina <53597131+itsina96@users.noreply.github.com>
2020-02-18 12:49:31 +0000

    add custom attributes for calculation

--- app.js
+++ app.js
@@ -9,32 +9,67 @@ keys.addEventListener('click', e => {
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType; 
         
-        // Replace the displayed number '0' with the clicked key.
         if(!action) {
-            if(displayedNum === '0') {
+            // Replace the displayed/default number '0' with the clicked key (also, after operator is applied).
+            if(displayedNum === '0' || previousKeyType === 'operator') {
                 display.textContent = keyContent;
             } else {
-                // basically, if the first clicked key isn't 0 and user clicks another key(s), we want to append it to the displayed number.
+                // basically, if the first clicked key isn't '0' && user clicks another key(s), we want to append it to the displayed number.
                 display.textContent = displayedNum + keyContent;
             }
         }
         
-        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-            console.log('operator key!')
+        // Concatenate/append '.' to the displayed number when clicked.
+        if (action === 'decimal') {
+            // Do nothing if string has a dot already 
+            if(!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.';
+            } 
         }
         
-        if (action === 'decimal') {
-            console.log('decimal key!')
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            // key.classList.add('depressed')
+            
+            // A custom attribute to check if the previous key is an operator before update the display to the clicked key.
+            calculator.dataset.previousKeyType = 'operator';
+            
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
         }
+        
+        // // Release the pressed state by removing 'depressed' class through a forEach loop.
+        // Array.from(key.parentNode.children).forEach(k => k.classList.remove('depressed'));
         
         if(action === 'clear') {
-            console.log('clear key!')
+            display.textContent = 0; //or '0' as a string??
+        }
+        
+        const calculate = (n1, operator, n2) => {
+            let result = '';
+            
+            // why parseFloat()? firstValue & secondValue are strings at this point. (1+1 = 11) parseFloat converts a string into a float (number with decimal places).
+            if (operator === 'add') {
+                result = parseFloat(n1) + parseFloat(n2);
+            } else if (operator === 'subtract') {
+                result = parseFloat(n1) - parseFloat(n2);
+            } else if (operator === 'multiply') {
+                result = parseFloat(n1) * parseFloat(n2);
+            } else if (operator === 'divide') {
+                result = parseFloat(n1) / parseFloat(n2);
+            }
+            return result;
         }
         
         if(action === 'calculate') {
-            console.log('equal key!')
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            
+            display.textContent = calculate(firstValue, operator, secondValue);
         }
+        
     }
     
 });
